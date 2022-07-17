3894f2efdb45456083e1edbf42b1f2610f6cb2a8
NicMortelliti <npmortelliti@gmail.com>
2022-07-17 09:58:39 -0700

    Change 'for' to 'htmlFor'

--- src/components/PasswordEntry.js
+++ src/components/PasswordEntry.js
@@ -3,7 +3,7 @@ import React from "react";
 function PasswordEntry() {
   return (
     <div>
-      <label for="pass">Password: </label>
+      <label htmlFor="pass">Password: </label>
       <input type="password" id="pass" name="password" className="entryField" />
     </div>
   );

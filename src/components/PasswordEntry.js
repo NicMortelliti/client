3175bab79273c8b0ae8993763827e7c695b108e7
NicMortelliti <npmortelliti@gmail.com>
2022-07-17 10:28:35 -0700

    Add unique names

--- src/components/PasswordEntry.js
+++ src/components/PasswordEntry.js
@@ -3,8 +3,13 @@ import React from "react";
 function PasswordEntry({ fieldName }) {
   return (
     <div>
-      <label htmlFor="pass">{`${fieldName}:`} </label>
-      <input type="password" id="pass" name="password" className="entryField" />
+      <label htmlFor={fieldName}>{`${fieldName}:`} </label>
+      <input
+        type="password"
+        id={fieldName}
+        name={fieldName}
+        className="entryField"
+      />
     </div>
   );
 }

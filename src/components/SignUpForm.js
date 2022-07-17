1d1325bb263b1d14d34ff6df4bb0bb96c32c7dc5
NicMortelliti <npmortelliti@gmail.com>
2022-07-17 13:14:44 -0700

    Check if user already exists

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -14,12 +14,32 @@ function SignUpForm() {
   // Handle form Submit
   const handleSubmitClick = (e) => {
     e.preventDefault();
+
+    // First check if both passwords match each other
+    //   If they don't, throw alert
+    // If they do, check if user exists in database
+    //   If they DO, throw alert
+    // If they don't, POST new user data to database
     if (formData.pass !== formData.repass) {
-      window.alert("Passwords do not match!");
+      // Passwords don't match
+      alert("Passwords do not match!");
       return;
     } else {
-      window.alert("Passwords DO match!");
-      return;
+      fetch(`http://localhost:9292/users/${formData.email}`)
+        .then((r) => r.json())
+        .then((result) => {
+          switch (result) {
+            // User does not yet exit
+            case null:
+              console.log("User not found");
+              break;
+
+            // User already exists
+            default:
+              alert("User already exists. Try logging in");
+              break;
+          }
+        });
     }
   };
 
